e3a7fadfef8b79feede43314621c624be29ac39c
Pranav Ramesh <pranav.ramesh1@gmail.com>
2024-09-20 23:12:27 -0400

    Implemented campaign issues

--- app/api/campaign/scrape/route.ts
+++ app/api/campaign/scrape/route.ts
@@ -1,9 +1,10 @@
 // Route to scrape websites for campaign data
 
-import { scrapeWebsites } from "./services";
+import { getCampaignIssues } from "./services";
 
 export async function POST(req: Request) {
     const { urls }: { urls: string[] } = await req.json();
-    const response = await scrapeWebsites(urls);
-    return Response.json({ response });
+    // const response = await scrapeWebsites(urls);
+    const campaignPolicies = await getCampaignIssues(urls);
+    return Response.json({ policies: campaignPolicies });
 }
--- app/api/campaign/scrape/services.ts
+++ app/api/campaign/scrape/services.ts
@@ -1,9 +1,12 @@
-import { Database } from "@/lib/types/schema";
 import FirecrawlApp, {
-    CrawlStatusResponse,
     ErrorResponse,
     ScrapeResponse,
 } from "@mendable/firecrawl-js";
+import { z } from "zod";
+import OpenAI from "openai";
+import { zodResponseFormat } from "openai/helpers/zod.mjs";
+import { APIPromise } from "openai/core.mjs";
+import { ParsedChatCompletion } from "openai/resources/beta/chat/completions.mjs";
 
 const scrapeWebsites = async (urls: string[]) => {
     const firecrawl = new FirecrawlApp({
@@ -28,19 +31,68 @@ function splitByH1(markdown: string) {
     return sections.filter((section) => section.trim() !== "");
 }
 
-type CampaignPolicy = Database["public"]["Tables"]["campaign_policies"]["Row"];
+const CampaignPolicySchema = z.object({
+    campaign_policy: z.string(),
+    campaign_subtopic: z.string(),
+    campaign_topic: z.string(),
+});
+
+const ListCampaignPolicySchema = z.object({
+    policies: z.array(CampaignPolicySchema),
+});
 
 const getCampaignIssues = async (urls: string[]) => {
     const data: (ScrapeResponse | ErrorResponse)[] = await scrapeWebsites(urls);
-    const issues: CampaignPolicy[] = [];
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const responses: APIPromise<
+        ParsedChatCompletion<z.infer<typeof ListCampaignPolicySchema>>
+    >[] = [];
+    const policies: z.infer<typeof CampaignPolicySchema>[] = [];
     for (const item of data) {
         if (item.error) {
             console.log(item.error);
         } else {
             const response_data = item as ScrapeResponse;
             const sections = splitByH1(response_data.markdown || "");
+            for (const section of sections) {
+                responses.push(openai.beta.chat.completions.parse(
+                    {
+                        model: "gpt-4o-mini",
+                        messages: [
+                            {
+                                role: "system",
+                                content:
+                                    "Given a description about a political candidate and their campaign, extract a list of the campaign's key policy positions.",
+                            },
+                            {
+                                role: "user",
+                                content: section,
+                            },
+                        ],
+                        response_format: zodResponseFormat(
+                            ListCampaignPolicySchema,
+                            "List of Campaign Policies",
+                        ),
+                    },
+                ));
+            }
+            console.log(responses.length, "responses to parse");
+            const parsed = await Promise.all(responses);
+            console.log(parsed.length, "parsed");
+            for (const policyList of parsed) {
+                if (policyList.choices[0].message.parsed) {
+                    policies.push(
+                        ...(policyList.choices[0].message.parsed as z.infer<
+                            typeof ListCampaignPolicySchema
+                        >).policies,
+                    );
+                }
+            }
         }
     }
+    return policies;
 };
 
-export { scrapeWebsites };
+export { getCampaignIssues, scrapeWebsites };
